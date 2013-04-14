8db7592eef6c13c5010f339cc189686778016477
Baptiste Mathus <batmat@batmat.net>
2013-04-14 08:57:17 +0200

    Added Link to github account.
    Improved the script a bit to use the github API to try
    finding the github accounts of the contributors.
    Manually fixed what couldn't be found.

--- froggies.sh
+++ froggies.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo "<itemizedlist>"
-git log --pretty=format:%an hudsonbook-content-fr/ | sort -u | while read line ;do
-   echo "  <listitem>$line</listitem>"
+git log --pretty=format:"%an" hudsonbook-content-fr/ | sort -u | while read line ;do
+	tosearch=`echo $line|sed "s/ /%20/g"`
+	result=`curl -s https://api.github.com/legacy/user/search/$tosearch | grep login | tr -s ' ' | cut -d'"' -f4`
+   echo "  <listitem><ulink url=\"http://github.com/$result\">$line</ulink></listitem>"
 done
 echo "</itemizedlist>"
 
